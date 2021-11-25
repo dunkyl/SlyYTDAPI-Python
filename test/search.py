9d5e1b1e92b3d146b4a8dd21345c4dac1c5f2c2f
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2021-11-24 22:34:28 -0800

    new tests and limit search results

---
+++ test/search.py
@@ -0,0 +1,12 @@
+import asyncio
+from SlyYTDAPI import *
+
+async def main():
+    yt = YouTubeData_WithMembers(open('api_key.txt').read())
+
+    search_results = await yt.search_videos('gangnam style', limit=5)
+
+    for video in search_results:
+        print(F"{video.title} by {video.channel_title}")
+
+asyncio.run(main())
